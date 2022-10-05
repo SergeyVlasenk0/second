b30e723004bc7c0be3c6281f0be4d856d2318289
Sergey Vlasenk0 <vlasenko_1990@internet.ru>
2022-10-05 14:11:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 100_000;
+
+        int miles = price / 20;
+        System.out.println("Бонус: " +miles);
+
+
+
+    }
+}
